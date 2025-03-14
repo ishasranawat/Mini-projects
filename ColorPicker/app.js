53727f365a4bca1a17c4ef128de6eebaac407ab3
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-14 15:18:28 +0530

    Create app.js

---
+++ ColorPicker/app.js
@@ -0,0 +1,18 @@
+let btn= document.querySelector("button");
+
+btn.addEventListener("click", function(){
+  let h3= document.querySelector('h3');
+  let randomColor= generateRandomColor();
+  h3.innerHTML= randomColor;
+  let divv = document.querySelector('div');
+  divv.style.backgroundColor = randomColor;
+});
+
+function generateRandomColor(){
+  let red= Math.floor(Math.random()*255);
+  let green= Math.floor(Math.random()*255);
+  let blue= Math.floor(Math.random()*255);
+  
+  let color= `rgb(${red}, ${green}, ${blue})`;
+  return color;
+}
