da46f58267346263c36af8736632aa180a7a2410
Isha <158483807+ishasranawat@users.noreply.github.com>
2025-03-19 12:46:49 +0530

    Update script.js

--- Car-Racing/script.js
+++ Car-Racing/script.js
@@ -1,1 +1,27 @@
+$(function() {
 
+    var anim_id;
+
+    //saving dom objects to variables
+    var container = $('#container');
+    var car = $('#car');
+    var car_1 = $('#car_1');
+    var car_2 = $('#car_2');
+    var car_3 = $('#car_3');
+    var line_1 = $('#line_1');
+    var line_2 = $('#line_2');
+    var line_3 = $('#line_3');
+    var restart_div = $('#restart_div');
+    var restart_btn = $('#restart');
+    var score = $('#score');
+    var high_score = localStorage.getItem('high_score');
+    $('#high_score').text(high_score);
+
+    //saving some initial setup
+    var container_left = parseInt(container.css('left'));
+    var container_width = parseInt(container.width());
+    var container_height = parseInt(container.height());
+    var car_width = parseInt(car.width());
+    var car_height = parseInt(car.height());
+
+    //some other declarations
